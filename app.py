745fcb453c02901737b150833dedf585823185d6
Nadja Machado <81447268+Nadjamac@users.noreply.github.com>
2021-07-07 23:15:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template,request
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql.elements import True_
 from werkzeug.datastructures import RequestCacheControl
 
 app = Flask(__name__)
 
-
+# Configurações de acesso ao banco de dados
 user = 'uutnfujn'
 password = 'TJ-budc9lkNSwDgt29vxcSjalpXx2bj7'
 host = 'tuffi.db.elephantsql.com'
@@ -15,47 +14,50 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{password}@{host}/
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "chave escondidamente"
 
-
+# Instanciando objeto da classe SQLAlchemy
 db = SQLAlchemy(app)
 
-
-class racas(db.Model): #Classe  que define a união e funcionamento do banco de dados no programa 
+# Modelar a Classe Filmes -> tabela racas
+class racas(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(255), nullable=False)
     imagem = db.Column(db.String(255), nullable=False)
-    curiosidade = db.Column(db.String(10000),nullable= False) # incluido mais uma coluna no banco de dados direcionando para o texto historias acionado pelo botao curiosidades.
+    curiosidade = db.Column(db.String(10000),nullable=False)
 
-    def __init__(self, nome, imagem,curiosidade):# definindo o construtor e o paramentro para as funções do banco de dados 
+    def __init__(self, nome, imagem, curiosidade):
         self.nome = nome
         self.imagem = imagem
         self.curiosidade = curiosidade
-    
+    # o static usa ,geralmente em uma manipulaçao que vai trazer algum registro do banco
     @staticmethod
-    def read_all():  # metodo statico que retorna de forma ascendente as informacoes os id da tabela do banco de dados 
+    def read_all():
+        # SELECT * FROM filmes ORDER BY id ASC
         return racas.query.order_by(racas.id.asc()).all()
-     db = SQLAlchemy(app)
+    db = SQLAlchemy(app)
 
     @staticmethod
     def read_single(id_registro):
-      
+        # SELECT * FROM filmes ORDER BY id ASC
         return racas.query.get(id_registro)
     db = SQLAlchemy(app)    
     
 
-    def save(self): 
+    def save(self): #função para salvar os dados inserido via input no bd, 
+    #não é staticmethod pq nao precisa de uma qyuery para ser chamado e adicionado
+        db.session.add(self)
         db.session.commit()
 
-    def update(self,novo_nome,novo_imagem,novo_curiosidade):# funcao que vai atualizar os registro recebido pelo usuario 
+    def update(self,novo_nome,novo_imagem,novo_curiosidade):
         self.nome = novo_nome
         self.imagem = novo_imagem
         self.curiosidade = novo_curiosidade
         self.save()
-
+        
     def delete(self):   # funcao que vai apagar o registro recebido pelo usuario na pagina pelo botão excluir 
         db.session.delete(self)
-        db.session.commit()   
+        db.session.commit()       
 
-@app.route("/")# definindo o caminho da pagina a rota que o programa deve realizar para cada funcão definida nos comandos def
+@app.route("/")
 def index():
     return render_template("index.html")
 
@@ -63,7 +65,7 @@ def index():
 @app.route("/read")
 def read_all():
     registros = racas.read_all()
-    # Chamada do método read_all da classe Raças, que representa a tabela  racas do banco de dados.
+    # Chamada do método read_all da classe racas, que representa a tabela racas do banco de dados.
     registros = racas.read_all()
     return render_template("read_all.html", registros=registros)
 
@@ -72,7 +74,7 @@ def read_all():
 def read_id(id_registro):
     registro= racas.read_single(id_registro)
     return render_template("read_single.html", registro=registro)
-    
+
 
 @app.route("/create", methods=('GET', 'POST'))
 def create():
@@ -90,15 +92,16 @@ def create():
 
 @app.route("/update/<id_registro>", methods=('GET', 'POST'))
 def update(id_registro):
-    sucesso = False 
+    sucesso = False # se foi alterado com sucesso ou nao
     registro= racas.read_single(id_registro)
 
     if request.method == 'POST':
         form = request.form
         registro.update(form['nome'], form['imagem'],form['curiosidade'])
+        # # novo_registro = racas(form['nome'], form['imagem'],form['curiosidade'])
+        # registro.update(novo_registro)
         sucesso = True
     return render_template('update.html',registro=registro,sucesso=sucesso) 
-
 
 @app.route('/delete/<id_registro>')
 def delete(id_registro):
@@ -116,6 +119,7 @@ def delete_confirmed(id_registro):
         confirmar=True
 
     return render_template('delete.html',registro=registro,confirmar=confirmar)  
+
 
 
 if (__name__ == "__main__"):
